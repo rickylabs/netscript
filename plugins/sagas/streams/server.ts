6496010877d62e47bc9ce89758f428dc24daa94f
Rickylabs <eric.chautems@gmail.com>
2026-06-09 19:54:00 +0200

    fix(plugin-sagas): publish stream type boundaries

--- plugins/sagas/streams/server.ts
+++ plugins/sagas/streams/server.ts
@@ -1,3 +1,15 @@
 export { getSagasStreamProducer, startSagasStreamMirror } from './producer.ts';
-export type { SagasStreamMirrorOptions, SagaStreamPrismaClient } from './producer.ts';
+export type {
+  SagaInstanceRecord,
+  SagaInstanceRecordSelect,
+  SagasStreamMirrorOptions,
+  SagaStreamPrismaClient,
+} from './producer.ts';
 export { SagaInstanceSchema, sagasStreamSchema } from './schema.ts';
+export type {
+  DurableStreamProducer,
+  SagasStreamDefinition,
+  StateSchema,
+  StreamProducerPort,
+  StreamStateDefinition,
+} from './producer.ts';
